f55ef0fdfd7453594f686e00ba59a53f0b83ddf2
rlaak <rlaak@LAPTOP-KJ6O4JV5>
2019-07-31 15:57:46 +0900

    s수정

--- JAVA_GIT/src/ch01/BTest.java
+++ JAVA_GIT/src/ch01/BTest.java
@@ -3,6 +3,6 @@ package ch01;
 public class BTest {
 	public static void main(String[] args) {
 		System.out.println("BTest First Commit");
-		System.out.println("BText Second Commit");
+		System.out.println("BTest Second Commit");
 	}
 }
